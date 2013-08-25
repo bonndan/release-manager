93228dde47c899108f9b4a96533ca28aba8efb30
Daniel Pozzi <bonndan76@googlemail.com>
2013-08-25 20:12:41 +0200

    fixed the init command test

--- test/Liip/RMT/Tests/Functional/InitCommandTest.php
+++ test/Liip/RMT/Tests/Functional/InitCommandTest.php
@@ -12,15 +12,16 @@ class InitCommandTest extends RMTFunctionalTestBase
     public function testInitConfig()
     {
         copy(__DIR__ . '/composer_no_rmt.json', $this->tempDir .'/composer.json');
-        exec('./RMT init --vcs=git --persister=vcs-tag -n');
-        
         $helper = new \Liip\RMT\Helpers\ComposerConfig();
         $helper->setComposerFile($this->tempDir .'/composer.json');
-        $config = $helper->getRMTConfigSection();
+        $this->assertNull($helper->getRMTConfigSection());
         
-        $this->assertNotNull($config);
-        $this->assertEquals('git', $config['vcs']);
-        $this->assertEquals('vcs-tag', $config['version-persister']);
+        exec('./RMT init --vcs=git --persister=vcs-tag -n');
+        
+        $config = $helper->getRMTConfigSection();
+        $this->assertNotNull();
+        $this->assertEquals('git', $config->getVcs());
+        $this->assertEquals('vcs-tag', $config->getVersionPersister());
     }
 }
 
