69083df240cbd2d520805b1536f4ccc231af9bf4
Daniel Pozzi <bonndan76@googlemail.com>
2013-08-25 19:35:15 +0200

    changed init command not to ask for version generator options (left over from simple versioning removal)

--- src/Liip/RMT/Command/InitCommand.php
+++ src/Liip/RMT/Command/InitCommand.php
@@ -1,10 +1,10 @@
 <?php
+
 namespace Liip\RMT\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
-
 use Liip\RMT\Information\InformationRequest;
 use Liip\RMT\Helpers\JSONHelper;
 
@@ -13,21 +13,37 @@ use Liip\RMT\Helpers\JSONHelper;
  */
 class InitCommand extends BaseCommand
 {
+    /**
+     * information collector
+     * 
+     * @var \Liip\RMT\Information\InformationCollector 
+     */
     protected $informationCollector;
     protected $executablePath;
+    
+    /**
+     * path to command.php
+     * @var string
+     */
     protected $commandPath;
+
+    /**
+     * path to the composer config file
+     * 
+     * @var string
+     */
     protected $configPath;
 
     protected function buildPaths()
     {
         $projectDir = $this->getApplication()->getProjectRootDir();
-        $this->executablePath = $projectDir.'/RMT';
-        $this->configPath = $projectDir.'/rmt.json';
-        $this->commandPath = realpath(__DIR__.'/../../../../command.php');
+        $this->executablePath = $projectDir . '/RMT';
+        $this->configPath = $projectDir . '/composer.json';
+        $this->commandPath = realpath(__DIR__ . '/../../../../command.php');
 
         // If possible try to generate a relative link for the command if RMT is installed inside the project
         if (strpos($this->commandPath, $projectDir) === 0) {
-            $this->commandPath = substr($this->commandPath, strlen($projectDir)+1);
+            $this->commandPath = substr($this->commandPath, strlen($projectDir) + 1);
         }
     }
 
@@ -37,9 +53,6 @@ class InitCommand extends BaseCommand
         $this->setDescription('Setup a new project configuration in the current directory');
         $this->setHelp('The <comment>init</comment> interactive task can be used to setup a new project');
 
-        // Add an option to force re-creation of the config file
-        $this->getDefinition()->addOption(new InputOption('force', null, InputOption::VALUE_NONE, 'Force update of the config file'));
-
         // Create an information collector and configure the different information request
         $this->informationCollector = new \Liip\RMT\Information\InformationCollector();
         $this->informationCollector->registerRequests(array(
@@ -47,23 +60,17 @@ class InitCommand extends BaseCommand
                 'description' => 'The VCS system to use',
                 'type' => 'choice',
                 'choices' => array('git', 'hg', 'none'),
-                'choices_shortcuts' => array('g'=>'git', 'h'=>'hg', 'n'=>'none'),
+                'choices_shortcuts' => array('g' => 'git', 'h' => 'hg', 'n' => 'none'),
                 'default' => 'none'
-            )),
-            new InformationRequest('generator', array(
-                'description' => 'The generator to use for version incrementing',
-                'type' => 'choice',
-                'choices' => array('semantic-versioning', 'basic-increment'),
-                'choices_shortcuts' => array('s'=>'semantic-versioning', 'b'=>'basic-increment'),
-            )),
+                )),
             new InformationRequest('persister', array(
                 'description' => 'The strategy to use to persist the current version value',
                 'type' => 'choice',
                 'choices' => array('vcs-tag', 'changelog'),
-                'choices_shortcuts' => array('t'=>'vcs-tag', 'c'=>'changelog'),
+                'choices_shortcuts' => array('t' => 'vcs-tag', 'c' => 'changelog'),
                 'command_argument' => true,
                 'interactive' => true
-            ))
+                ))
         ));
         foreach ($this->informationCollector->getCommandOptions() as $option) {
             $this->getDefinition()->addOption($option);
@@ -80,16 +87,18 @@ class InitCommand extends BaseCommand
         $this->buildPaths();
 
         // Security check
-        if (file_exists($this->configPath) && $input->getOption('force')!==true) {
-            throw new \Exception("A rmt.json file already exist, if you want to regenerate it, use the --force option");
+        $helper = new \Liip\RMT\Helpers\ComposerConfig($this->getContext());
+        $section = $helper->getRMTConfigSection();
+        if ($section !== null) {
+            throw new \Exception('A config section "extra/rmt" already exists in composer json.');
         }
     }
 
-    protected function interact(InputInterface $input, OutputInterface $output){
-
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
         // Fill up questions
-        if ($this->informationCollector->hasMissingInformation()){
-            foreach($this->informationCollector->getInteractiveQuestions() as $name => $question) {
+        if ($this->informationCollector->hasMissingInformation()) {
+            foreach ($this->informationCollector->getInteractiveQuestions() as $name => $question) {
                 $answer = $this->askQuestion($question);
                 $this->informationCollector->setValueFor($name, $answer);
                 $this->writeEmptyLine();
@@ -101,39 +110,36 @@ class InitCommand extends BaseCommand
     {
         // Create the executable task inside the project home
         $this->getOutput()->writeln("Creation of the new executable <info>{$this->executablePath}</info>");
-        file_put_contents($this->executablePath,
-            "#!/usr/bin/env php\n".
-            "<?php define('RMT_ROOT_DIR', __DIR__); ?>\n".
+        file_put_contents($this->executablePath, "#!/usr/bin/env php\n" .
+            "<?php define('RMT_ROOT_DIR', __DIR__); ?>\n" .
             "<?php require '{$this->commandPath}'; ?>\n"
         );
         exec('chmod +x RMT');
 
         // Create the config file
-        $this->getOutput()->writeln("Creation of the config file <info>{$this->configPath}</info>");
-        file_put_contents(
-            $this->configPath,
-            JSONHelper::format(json_encode($this->getConfigData()))
-        );
+        $composerHelper = new \Liip\RMT\Helpers\ComposerConfig();
+        $composerHelper->setComposerFile($this->configPath);
+        $composerHelper->addRMTConfigSection(\Liip\RMT\Config::create($this->getConfigData()));
+        $this->getOutput()->writeln("Added extra/rmt section to <info>{$this->configPath}</info>");
 
         // Confirmation
         $this->writeBigTitle('Success, you can start using RMT by calling <info>RMT release</info>');
         $this->writeEmptyLine();
     }
 
-    public function getConfigData() {
+    public function getConfigData()
+    {
         $config = array();
 
         $vcs = $this->informationCollector->getValueFor('vcs');
-        if ($vcs !== 'none'){
+        if ($vcs !== 'none') {
             $config['vcs'] = $vcs;
         }
-
-        $generator = $this->informationCollector->getValueFor('generator');
-        $config['version-generator'] = $generator == 'semantic-versioning' ? 'semantic' : 'simple';
 
         $config['version-persister'] = $this->informationCollector->getValueFor('persister');
 
         return $config;
     }
+
 }
 
--- test/Liip/RMT/Tests/Functional/InitCommandTest.php
+++ test/Liip/RMT/Tests/Functional/InitCommandTest.php
@@ -5,17 +5,22 @@ namespace Liip\RMT\Tests\Functional;
 
 class InitCommandTest extends RMTFunctionalTestBase
 {
+    /**
+     * Ensures that the composer file is extended with the rmt config data.
+     * 
+     */
     public function testInitConfig()
     {
-        unlink('rmt.json');
-        $this->assertFileNotExists('rmt.json');
-//        $this->manualDebug();
-        exec('./RMT init --vcs=git --generator=semantic-versioning --persister=vcs-tag -n');
-        $this->assertFileExists('rmt.json');
-        $config = json_decode(file_get_contents('rmt.json'), true);
+        copy(__DIR__ . '/composer_no_rmt.json', $this->tempDir .'/composer.json');
+        exec('./RMT init --vcs=git --persister=vcs-tag -n');
+        
+        $helper = new \Liip\RMT\Helpers\ComposerConfig();
+        $helper->setComposerFile($this->tempDir .'/composer.json');
+        $config = $helper->getRMTConfigSection();
+        
+        $this->assertNotNull($config);
         $this->assertEquals('git', $config['vcs']);
         $this->assertEquals('vcs-tag', $config['version-persister']);
-        $this->assertEquals('semantic', $config['version-generator']);
     }
 }
 
