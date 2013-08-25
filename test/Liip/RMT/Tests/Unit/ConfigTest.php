6871e53bbaa04d0bc23195ac2727af05728425a1
Daniel Pozzi <bonndan76@googlemail.com>
2013-08-25 22:24:03 +0200

    improved the config tests

--- test/Liip/RMT/Tests/Unit/ConfigTest.php
+++ test/Liip/RMT/Tests/Unit/ConfigTest.php
@@ -7,9 +7,26 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
 {
     public function testFactoryReturnsConfig()
     {
-        $config = Config::create(array('vcs' => 'git'));
+        $config = Config::create($this->getConfigData());
         
         $this->assertInstanceOf("\Liip\RMT\Config", $config);
         $this->assertEquals('git', $config->getVcs());
+        $this->assertContains('working-copy-check', $config->getPrerequisites());
+    }
+    
+    public function testToJson()
+    {
+        $config = Config::create($this->getConfigData());
+        $json = $config->toJson();
+        $this->assertEquals('git', $json->vcs);
+        $this->assertContains('working-copy-check',$json->prerequisites);
+    }
+    
+    protected function getConfigData()
+    {
+        return array(
+            'vcs' => 'git',
+            'prerequisites' => array('working-copy-check')
+        );
     }
 }
