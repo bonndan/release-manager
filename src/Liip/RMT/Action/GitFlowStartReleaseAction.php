a889375967e2e9bf20984c03e363c0346cfc81b2
Daniel Pozzi <bonndan76@googlemail.com>
2014-03-06 22:11:00 +0100

    git flow actions, one finish action for both branch types

--- src/Liip/RMT/Action/GitFlowStartReleaseAction.php
+++ src/Liip/RMT/Action/GitFlowStartReleaseAction.php
@@ -1,20 +1,17 @@
 <?php
 namespace Liip\RMT\Action;
 
-use Liip\RMT\Action\BaseAction;
-
 /**
  * Creates a new git flow release branch.
  *
  * @author Daniel Pozzi <bonndan76@googlemail.com>
  */
-class GitFlowStartReleaseAction extends BaseAction
+class GitFlowStartReleaseAction extends GitFlowAction
 {
     public function execute()
     {
         $newVersion = $this->context->getNewVersion();
-        $git = $this->context->getVCS();
-        $git->startRelease($newVersion);
+        $this->getVCS()->startRelease($newVersion);
         $this->context->getOutput()->writeln('Created a new git flow release ' . $newVersion);
     }
 }
--- test/Liip/RMT/Tests/Unit/Action/GitFlowFinishActionTest.php
+++ test/Liip/RMT/Tests/Unit/Action/GitFlowFinishActionTest.php
@@ -1,8 +1,14 @@
 <?php
 namespace Liip\RMT\Tests\Unit\Changelog;
 
+use Liip\RMT\Action\GitFlowFinishAction;
+use Liip\RMT\Context;
+use Liip\RMT\Exception;
+use Liip\RMT\Version\Detector\GitFlowBranch;
+use PHPUnit_Framework_TestCase;
 
-class GitFlowFinishReleaseActionTest extends \PHPUnit_Framework_TestCase
+
+class GitFlowFinishActionTest extends PHPUnit_Framework_TestCase
 {
     private $context;
     private $git;
@@ -16,14 +22,19 @@ class GitFlowFinishReleaseActionTest extends \PHPUnit_Framework_TestCase
     
     public function setUp()
     {
+        $this->prepareAction(GitFlowBranch::RELEASE);
+    }
+    
+    private function prepareAction($branchType)
+    {
         $this->git = $this->getMock("\Liip\RMT\VCS\GitFlow");
         
-        $this->context = new \Liip\RMT\Context();
+        $this->context = new Context();
         $this->context->setService('vcs', $this->git);
         $this->context->setService('output', $this->getMock("\Liip\RMT\Output\Output"));
         $this->informationCollector = $this->getMock("\Liip\RMT\Information\InformationCollector");
         $this->context->setService('information-collector', $this->informationCollector);
-        $this->action = new \Liip\RMT\Action\GitFlowFinishReleaseAction();
+        $this->action = new GitFlowFinishAction($branchType);
         $this->action->setContext($this->context);
     }
     
@@ -38,6 +49,22 @@ class GitFlowFinishReleaseActionTest extends \PHPUnit_Framework_TestCase
                 ->method('finishRelease')
                 ->with($comment)
                 ->will($this->returnValue('release/1.2.3'));
+        
+        $this->action->execute();
+    }
+    
+    public function testFinishHotfix()
+    {
+        $this->prepareAction(GitFlowBranch::HOTFIX);
+        $comment = 'test';
+        $this->informationCollector->expects($this->once())
+                ->method('getValueFor')
+                ->will($this->returnValue($comment));
+        
+        $this->git->expects($this->once())
+                ->method('finishHotfix')
+                ->with($comment)
+                ->will($this->returnValue('hotfix/1.2.3'));
         
         $this->action->execute();
     }
@@ -66,7 +93,7 @@ class GitFlowFinishReleaseActionTest extends \PHPUnit_Framework_TestCase
         $this->git->expects($this->once())
                 ->method('finishRelease')
                 ->with($comment)
-                ->will($this->throwException(new \Liip\RMT\Exception('test')));
+                ->will($this->throwException(new Exception('test')));
         
         $this->setExpectedException("\Liip\RMT\Exception");
         $this->action->execute();
