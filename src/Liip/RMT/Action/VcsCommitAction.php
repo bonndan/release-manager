dc2325bd118dac6bc398fe4c386cc4ccd25194f8
Daniel Pozzi <bonndan76@googlemail.com>
2014-03-18 20:04:22 +0100

    #12 the automatically added post - release commit action fails gracefully

--- src/Liip/RMT/Action/VcsCommitAction.php
+++ src/Liip/RMT/Action/VcsCommitAction.php
@@ -2,17 +2,45 @@
 
 namespace Liip\RMT\Action;
 
+use Liip\RMT\Exception;
+
 /**
  * Commit everything
  */
 class VcsCommitAction extends BaseAction
 {
+    /**
+     * flag to enable graceful fails
+     * @var boolean
+     */
+    private $failsGracefully = false;
+    
     public function execute()
     {
-        $this->context->getVCS()->saveWorkingCopy(
-            'Release of new version '. $this->context->getParam('new-version')
-        );
+        try {
+            $this->context->getVCS()->saveWorkingCopy(
+                'Release of new version '. $this->context->getParam('new-version')
+            );
+        } catch (Exception $ex) {
+            if ($this->failsGracefully) {
+                $this->confirmSuccess('Failed gracefully: ' . $ex->getMessage());
+                return;
+            }
+            
+            throw $ex;
+        }
+        
         $this->confirmSuccess();
+    }
+    
+    /**
+     * Toggle graceful failing.
+     * 
+     * @param boolean $flag
+     */
+    public function setFailsGracefully($flag)
+    {
+        $this->failsGracefully = (boolean) $flag;
     }
 }
 
