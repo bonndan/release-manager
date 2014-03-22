06f0664b1eb833f521a8e73527812da201133fd4
Daniel Pozzi <bonndan76@googlemail.com>
2014-03-22 17:24:49 +0100

    extraced a command test case

---
+++ test/Liip/RMT/Tests/Unit/Command/CommandTestCase.php
@@ -0,0 +1,47 @@
+<?php
+namespace Liip\RMT\Tests\Unit\Command;
+
+use Liip\RMT\Application;
+use Liip\RMT\Context;
+use PHPUnit_Framework_TestCase;
+
+
+/**
+ * CommandTestCase
+ *
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ */
+class CommandTestCase extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Application
+     */
+    protected $application;
+    
+    /**
+     * @var Context
+     */
+    protected $context;
+    
+    public function setUp()
+    {
+        $this->application = new Application();
+        $this->context = Context::create($this->application);
+        $this->context->setService('information-collector', $this->getMock("\Liip\RMT\Information\InformationCollector"));
+    }
+    
+    protected function runCommand()
+    {
+        $input = $this->getMock("\Symfony\Component\Console\Input\InputInterface");
+        $output = $this->getMock("\Liip\RMT\Output\Output");
+        try {
+            $this->command->run($input, $output);
+        } catch (\Exception $ex) {
+            if (strpos($ex->getMessage(), 'local modifications') !== false) {
+                return;
+            } else {
+                throw $ex;
+            }
+        }
+    }
+}
